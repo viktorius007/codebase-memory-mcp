4607b158914422e8ae664c99d04e990f117aab4b
Martin Vogel <martin.vogel.tech@gmail.com>
2026-06-27 02:27:54 +0200

    test: extend TEMP AST-dump with batch-2 grammars
    Signed-off-by: Martin Vogel <martin.vogel.tech@gmail.com>

--- tests/repro/repro_ast_dump.c
+++ tests/repro/repro_ast_dump.c
@@ -73,4 +73,35 @@ void suite_repro_ast_dump(void) {
              "module M exposing (..)\nadd a = a\nx = add 1\n");
     dump_ast("agda", CBM_LANG_AGDA, "add : Nat -> Nat\nadd a = add a\n");
     dump_ast("nim", CBM_LANG_NIM, "proc add(a: int): int = a\nlet x = add(1)\n");
+
+    /* Batch 2: def-label + call + QN grammars. */
+    dump_ast("jsonnet", CBM_LANG_JSONNET, "local add(a) = a;\n{ x: add(1) }\n");
+    dump_ast("nickel", CBM_LANG_NICKEL, "let add = fun a => a in add 1\n");
+    dump_ast("typst", CBM_LANG_TYPST, "#let add(a) = a\n#add(1)\n");
+    dump_ast("magma", CBM_LANG_MAGMA, "function add(a)\n  return a;\nend function;\nx := add(1);\n");
+    dump_ast("meson", CBM_LANG_MESON, "project('x')\nadd = executable('a', 'a.c')\n");
+    dump_ast("zig", CBM_LANG_ZIG,
+             "const S = struct { x: i32 };\nfn add(a: i32) i32 { return a; }\n");
+    dump_ast("graphql", CBM_LANG_GRAPHQL, "type Query {\n  add(a: Int): Int\n}\n");
+    dump_ast("prisma", CBM_LANG_PRISMA, "model User {\n  id Int @id\n  name String\n}\n");
+    dump_ast("pony", CBM_LANG_PONY, "class C\n  fun add(a: U64): U64 => a\n");
+    dump_ast("dockerfile", CBM_LANG_DOCKERFILE, "FROM alpine\nENV X=1\nRUN echo hi\n");
+    dump_ast("gomod", CBM_LANG_GOMOD, "module example.com/x\n\nrequire foo v1.0.0\n");
+    dump_ast("css", CBM_LANG_CSS, ".x { background: url(a.png); }\n");
+    dump_ast("nasm", CBM_LANG_NASM, "inner:\n  ret\nouter:\n  call inner\n");
+    dump_ast("tlaplus", CBM_LANG_TLAPLUS, "---- MODULE M ----\nAdd(a) == a\nB == Add(1)\n====\n");
+    dump_ast("makefile", CBM_LANG_MAKEFILE, "add:\n\techo hi\n");
+    dump_ast("ini", CBM_LANG_INI, "[section]\nkey = val\n");
+    dump_ast("markdown", CBM_LANG_MARKDOWN, "# Heading\n\nsome text\n");
+    dump_ast("just", CBM_LANG_JUST, "build:\n    echo hi\n");
+    dump_ast("vhdl", CBM_LANG_VHDL,
+             "entity e is end;\narchitecture a of e is\n  function add(x: integer) return integer is\n"
+             "  begin return x; end;\nbegin end;\n");
+    dump_ast("systemverilog", CBM_LANG_SYSTEMVERILOG,
+             "module m;\n  function int add(int a); return a; endfunction\nendmodule\n");
+    dump_ast("func", CBM_LANG_FUNC, "int add(int a) { return a; }\n");
+    dump_ast("puppet", CBM_LANG_PUPPET, "define add($a) {}\nadd { 'x': a => 1 }\n");
+    dump_ast("smali", CBM_LANG_SMALI,
+             ".class public LFoo;\n.super Ljava/lang/Object;\n.field public x:I\n");
+    dump_ast("pkl", CBM_LANG_PKL, "class Foo {\n  x: Int\n}\n");
 }
